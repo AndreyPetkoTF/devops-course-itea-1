d0e7c97dbb79142c7c4f2c52f5255bd396f4f329
AndreyPetkoTF <andrey@stryber.com>
2019-07-16 15:06:49 +0300

    AP | update groovy job file

--- jenkins/jobs/docker_nginx_build.groovy
+++ jenkins/jobs/docker_nginx_build.groovy
@@ -7,7 +7,14 @@ job("docker-nginx-build") {
   }
   steps {
     def cmd = '''#!/bin/bash +x
-    ls
+    NODE_VERSION="v8.14.1"
+    if [ ! -d "$WORKSPACE/node-$NODE_VERSION-linux-x64" ]; then
+      wget https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz
+      tar xf node-$NODE_VERSION-linux-x64.tar.xz
+    fi
+    export PATH=$WORKSPACE/node-$NODE_VERSION-linux-x64/bin/:$PATH
+    npm cache clean --force
+    '''.stripIndent()
 
     shell(cmd)
   }
