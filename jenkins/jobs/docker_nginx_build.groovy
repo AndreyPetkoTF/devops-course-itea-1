79754bc45c4dc2eb74ac27db54dc40f4245714b6
AndreyPetkoTF <andrey@stryber.com>
2019-07-16 15:08:49 +0300

    AP | add only ls for docker nginx build

--- jenkins/jobs/docker_nginx_build.groovy
+++ jenkins/jobs/docker_nginx_build.groovy
@@ -7,13 +7,7 @@ job("docker-nginx-build") {
   }
   steps {
     def cmd = '''#!/bin/bash +x
-    NODE_VERSION="v8.14.1"
-    if [ ! -d "$WORKSPACE/node-$NODE_VERSION-linux-x64" ]; then
-      wget https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz
-      tar xf node-$NODE_VERSION-linux-x64.tar.xz
-    fi
-    export PATH=$WORKSPACE/node-$NODE_VERSION-linux-x64/bin/:$PATH
-    npm cache clean --force
+    ls
     '''.stripIndent()
 
     shell(cmd)
