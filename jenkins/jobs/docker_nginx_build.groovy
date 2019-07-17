fad41966ebcb098a5116192ebfaef348db9e83c6
AndreyPetkoTF <andrey@stryber.com>
2019-07-17 11:09:05 +0300

    AP | add docker build

--- jenkins/jobs/docker_nginx_build.groovy
+++ jenkins/jobs/docker_nginx_build.groovy
@@ -7,7 +7,12 @@ job("docker-nginx-build") {
   }
   steps {
     def cmd = '''#!/bin/bash +x
-    docker ps
+    git clone https://github.com/AndreyPetkoTF/devops-course-itea
+    cd devops-course-itea/nginx
+    docker build -t my-nginx .
+    docker login -u andreypetko -p 1qazse4
+    docker tag my-nginx andreypetko/my-nginx
+    docker push andreypetko/my-nginx
     '''.stripIndent()
 
     shell(cmd)
